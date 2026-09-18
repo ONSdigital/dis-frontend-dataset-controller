6de6e3156ae8a1adeff0985b275147d7a2e30fe2
fran moore <franmoore05@gmail.com>
2026-09-18 14:16:50 +0100

    Adding hero banner

--- dis_frontend_dataset_controller/flaskr/__init__.py
+++ dis_frontend_dataset_controller/flaskr/__init__.py
@@ -12,6 +12,90 @@ class PageConfigDict(TypedDict):
     meta: dict
     footer: dict
 
+class PanelLinkDict(TypedDict):
+    text: str
+    url: str
+    ariaLabel: str
+
+class PanelDict(TypedDict):
+    panelText: str
+    panelType: str
+    panelLink: PanelLinkDict
+
+class DescriptionListDict(TypedDict):
+    termCol: str
+    descriptionCol: str
+    itemsList: list[ItemDict]
+
+class ItemDict(TypedDict):
+    term: str
+    descriptions: list[DescriptionDict]
+
+class DescriptionDict(TypedDict):
+    description: str
+
+class OnsHeroDict(TypedDict):
+    variants: str
+    detailsColumns: str
+    officialStatisticsBadge: bool
+    officialStatisticsBadgeUrl: str
+    informationPanel: PanelDict
+    topic: str
+    title: str
+    text: str
+    descriptionList: DescriptionListDict
+
+
+def build_hero_banner() -> OnsHeroDict:
+    return {
+        "variants": 'grey',
+        "detailsColumns": '12',
+        "officialStatisticsBadge": 'true',
+        "officialStatisticsBadgeUrl": 'https://uksa.statisticsauthority.gov.uk/about-the-authority/uk-statistical-system/types-of-official-statistics/',
+        "informationPanel":{
+            "panelText": 'Latest release',
+            "panelType": 'ons-green',
+            "panelLink": {
+                "text": 'View previous releases',
+                "url": '#0',
+                "ariaLabel": 'View previous release of Retail sales rise amid summer discounts and sporting events'
+            }
+        },
+        "topic": 'Dataset',
+        "title": 'Dataset title goes here',
+        "text":  'Dataset description goes here',
+        "censusLogo": 'false',
+        "descriptionList": {
+            "termCol": "5",
+            "descriptionCol": "7",
+            "itemsList": [
+                {
+                    "term": "Released:",
+                    "descriptions": [
+                        {
+                            "description": "16 August 2024"
+                        }
+                    ]
+                },
+                {
+                    "term": "Last Updated:",
+                    "descriptions": [
+                        {
+                            "description": "20 September 2024"
+                        }
+                    ]
+                },
+                {
+                    "term": "Version:",
+                    "descriptions": [
+                        {
+                            "description": "1"
+                        }
+                    ]
+                }
+            ]
+        }
+    }
 
 def build_page_config() -> PageConfigDict:
     return {
@@ -55,6 +139,6 @@ def create_app() -> Flask:
     # a simple page that says hello
     @app.route("/hello")
     def hello() -> str:
-        return render_template("hello.html", pageconfig=build_page_config())
+        return render_template("hello.html", pageconfig=build_page_config(),herobanner=build_hero_banner())
 
     return app
